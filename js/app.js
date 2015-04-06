1cf2c7304c9c92e00182cc29b0ee49723d54346f
priyankakumbha <pramodkumbhar789@gmail.com>
2015-04-06 12:14:19 +1000

    given the number limit in js and changed h2 content

--- js/app.js
+++ js/app.js
@@ -5,47 +5,38 @@ $( "#addNumbers" ).keypress(function(e) {
 		var itemvalue= + itemvalueString;
       $(".resultArea" ).empty();
       $("#addNumbers").val('');
-		if (isNaN(itemvalue)){
-			
-        
-         $( ".resultArea" ).append("please enter a valid number");
-		}else{
 
-
-		console.log(itemvalue);
-		var resultstring= generateFizzBuzz(itemvalue)
-
-	 
-
-   console.log(resultstring);
- $( ".resultArea" ).append( $(resultstring) );
- 
-	}
-}
-
+		if (isNaN(itemvalue)) {
+		$( ".resultArea" ).append("please enter a valid number");
+		} else if (itemvalue>100) {
+      $( ".resultArea" ).append("please enter a number between 1 to 100");
+      }else {
+      console.log(itemvalue);
+      var resultstring= generateFizzBuzz(itemvalue) 
+       console.log(resultstring);
+      $( ".resultArea" ).append( $(resultstring) );
+      }
+   }
 });
-
 });
-function generateFizzBuzz(itemvalue){
+function generateFizzBuzz(itemvalue) {
    var resultstring='<p>' ;
 
    for (var i = 1; i <=itemvalue; i++) {
    
-   if(i%3==0 && i%5==0){
+   if(i%3==0 && i%5==0) {
      
-      resultstring=resultstring+" fizzbuzz  ";
-   }else if(i%3==0){
+   resultstring=resultstring+" fizzbuzz  ";
+   }else if(i%3==0) {
       
-         resultstring=resultstring+" fizz  ";
-   }else if(i%5==0){
+   resultstring=resultstring+" fizz  ";
+   }else if(i%5==0) {
       
-         resultstring=resultstring+" buzz  ";
-   }else{ 
-         resultstring=resultstring+ "  "+i;
+   resultstring=resultstring+" buzz  ";
+   }else { 
+   resultstring=resultstring+ "  "+i;
 
    }
-   
-
 }
 resultstring= resultstring +'</p>';
 return resultstring;
