2665febbee36b167721dfb75377ba5821d1997a3
priyankakumbha <priyankakumbhar789@gmail.com>
2015-08-12 21:03:01 +1000

    final touch

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ $( "#addNumbers" ).keypress(function(e) {
 	if (e.keyCode == 13) {
 		var itemvalueString = $("#addNumbers" ).val();
 		var itemvalue= + itemvalueString;
+       $(".message").empty();
       $(".resultArea" ).empty();
       $("#addNumbers").val('');
 
 		if (isNaN(itemvalue)) {
 		$( ".resultArea" ).append("please enter a valid number");
 		} else if (itemvalue>100) {
-      $( ".resultArea" ).append("please enter a number between 1 to 100");
+      $( ".message" ).append(itemvalue+ " not between 1 to 100");
       }else {
       console.log(itemvalue);
       var resultstring= generateFizzBuzz(itemvalue) 
@@ -20,24 +21,27 @@ $( "#addNumbers" ).keypress(function(e) {
 });
 });
 function generateFizzBuzz(itemvalue) {
-   var resultstring='<p>' ;
+   var resultstring="<p> <table class='resultTable'> <tr>" ;
 
    for (var i = 1; i <=itemvalue; i++) {
    
    if(i%3==0 && i%5==0) {
      
-   resultstring=resultstring+"<label id='fizbuzz'> fizzbuzz </label> ";
+   resultstring=resultstring+" <td> <label id='fizbuzz'> fizzbuzz </label> </td>";
    }else if(i%3==0) {
       
-   resultstring=resultstring+"<label> fizz </label>";
+   resultstring=resultstring+"<td> <label> fizz </label> </td>";
    }else if(i%5==0) {
       
-   resultstring=resultstring+"<label> buzz </label> ";
+   resultstring=resultstring+"<td> <label> buzz </label> </td>";
    }else { 
-   resultstring=resultstring+ "<label>"+i +"</label>";
+   resultstring=resultstring+ "<td> <label>"+" " + i +"</label> </td>";
 
+   }if(i%10==0){
+      resultstring=resultstring+"</tr> <tr>";
    }
+
 }
-resultstring= resultstring +'</p>';
+resultstring= resultstring +' </tr></table> </p>';
 return resultstring;
 }
